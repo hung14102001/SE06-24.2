27b5a2419f4ebea8340034aac3d5b9afaad4b459
themanh01 <nguyenthemanh_t64@hus.edu.vn>
2021-12-13 22:15:46 +0700

    resolve #8

---
+++ code/web/src/program-rust/src/gych.rs
@@ -0,0 +1,17 @@
+use borsh::{BorshDeserialize, BorshSerialize};
+use solana_program::{
+    account_info::{next_account_info, AccountInfo},
+    entrypoint,
+    entrypoint::ProgramResult,
+    msg,
+    program_error::ProgramError,
+    pubkey::Pubkey,
+};
+use spl_token::process::Processor;
+
+entrypoint!(process_instruction);
+
+pub fn process_instruction(
+
+
+)
